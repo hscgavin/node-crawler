c4ffce3635757cfab1918a04595d1205b0d68291
Gavin He <hscgavin@gmail.com>
2020-05-29 11:38:40 +1000

    fix: Adjust paddings to align header

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const superagent = require('superagent');
-const charset = require('superagent-charset')
+const charset = require('superagent-charset');
 
 charset(superagent);
 
@@ -10,36 +10,42 @@ const express = require('express');
 const baseUrl = 'https://seek.com.au';
 
 const app = express();
-app.get('/index', function(req, res) {
+app.get('/index', function (req, res) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
   const page = req.query.page || '1';
 
-  superagent.get(`${baseUrl}/jobs?page=${page}`)
+  superagent
+    .get(`${baseUrl}/jobs?page=${page}`)
     .charset('utf-8')
-    .end(function(err, sres) {
+    .end(function (err, sres) {
       const jobs = [];
       if (err) {
         console.log(`Error: ${err}`);
-        res.json({ code: 400, msg: err, jobs: jobs})
+        res.json({ code: 400, msg: err, jobs: jobs });
       }
+
       const $ = cheerio.load(sres.text);
-      $('[data-automation="searchResults"] article').each(function(idx, element){
+      console.log($('body').text());
+      $('[data-automation="searchResults"] article').each(function (
+        idx,
+        element
+      ) {
         const el = $(element);
         const a = el.find('h1 a');
         jobs.push({
           title: a.text(),
-          href: `${baseUrl}${a.attr('href')}`
+          href: `${baseUrl}${a.attr('href')}`,
         });
       });
       res.json({ code: 200, data: jobs });
     });
 });
 
-const server = app.listen(8081, function(){
+const server = app.listen(8081, function () {
   const host = server.address().address;
   const port = server.address().port;
   console.log(`visit http://${host}:${port}`);
